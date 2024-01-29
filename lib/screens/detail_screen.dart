47726c12548f526171235c84001d85fdaa5e84fc
junghyunsoo24 <sunni303356@naver.com>
2024-01-29 18:50:46 +0900

    fix: image do not reloaded

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -47,7 +47,9 @@ class DetailScreen extends StatelessWidget {
                       )
                     ],
                   ),
-                  child: Image.network(thumb),
+                  child: Image.network(
+                    thumb,
+                    headers: const {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",},),
                 ),
               ),
             ],
