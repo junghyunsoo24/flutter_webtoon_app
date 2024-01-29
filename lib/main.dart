139594baeabe8eaf4359e14f85ec026ce82a136a
junghyunsoo24 <sunni303356@naver.com>
2024-01-29 18:51:57 +0900

    fix: web's mouse scroll do not apply

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:webtoon_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    // etc.
+  };
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: MyCustomScrollBehavior(),
       home: HomeScreen(),
     );
   }
